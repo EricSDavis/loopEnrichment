61ae5000d008da3b03689d83fe6919c4a0ca4eac
Eric Davis <ericscottdavis@outlook.com>
2023-02-16 10:32:52 -0500

    Add code for looking at subsets of loops by distance.

--- scripts/loopEnrichmentByDistance.R
+++ scripts/loopEnrichmentByDistance.R
@@ -23,6 +23,8 @@ loops <-
 ## Check that all data is in the upper triangular
 all(start1(loops) < start2(loops))
 
+## Extract and calculate loop enrichment ---------------------------------------
+
 ## Extract matrices around each loop
 mats <- 
   pixelsToMatrices(x=loops, buffer=buffer) |>
@@ -59,12 +61,24 @@ bg <- apply(counts(mats), c(3,4), \(x) median(x[idx] + 1))
 interactions(mats)$score <- cp / bg[,1]
 interactions(mats)$size <- pairdist(mats)
 
-## Visualize trend
+## Visualize trend -------------------------------------------------------------
+
+## Data table of loop size and score
 dat <- data.table(
   size = interactions(mats)$size,
   scores = interactions(mats)$score
 )
 dat <- dat[order(size)]
+
+## Define threshold
+thresh <- 300e3
+
+## What perc of loops are >thresh
+tb <- table(dat$size > thresh)
+(tb[2] / sum(tb)) * 100
+
+## Filter out long (>2Mb) interactions
+dat <- dat[size <= thresh]
 
 ns <- round(seq(1, 200, length.out = 9))
 par(mfrow=c(3,3))
@@ -73,5 +87,7 @@ for (n in ns) {
   plot(dat$size, dat$rollMeds, type="l", main=paste0("n=", n))
 }
 par(mfrow=c(1,1))
+
+
 
 
